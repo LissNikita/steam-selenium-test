ebe29da01416c411cf8a4aea4a9c17febd509078
LissNikita <dragonlisov@mail.ru>
2024-01-25 10:28:17 +0300

    UPDATE for UROD
    UPDATE for UROD

--- src/main/java/Urod.java
+++ src/main/java/Urod.java
@@ -1,3 +1,4 @@
+package org.example;
 
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
@@ -13,8 +14,8 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 public class Urod {
 
     private static final String URL = "https://aviso.bz/";
-    private static final String LOGIN = "";
-    private static final String PASSWORD = "";
+    private static final String LOGIN = "dragonlisov";
+    private static final String PASSWORD = "lisovdragon1999";
     private static SelenideElement timeForWatchingText = $x("//*[@class = 'go-link-youtube']/ancestor::div//span[@class='serf-text']");
 
     public static void main(String[] args) {
@@ -55,6 +56,7 @@ public class Urod {
 
         while (true) {
             Selenide.refresh();
+            Selenide.sleep(2000);
             enrFromYouTubeButton.click();
             Selenide.sleep(2000);
             if (firstButtonForOpenLink.isDisplayed()) {
@@ -63,6 +65,7 @@ public class Urod {
                 Selenide.sleep(2000);
                 if(errorLink.isDisplayed()){
                     Selenide.refresh();
+                    Selenide.sleep(3000);
                 }else {
                     if (linkForYouTubeVideoButton.scrollTo().isDisplayed() && linkForYouTubeVideoButton.getCssValue("background").contains("rgb(96, 138, 185)")) {
                         int timeWaiting = timeForWaiting();
@@ -70,18 +73,18 @@ public class Urod {
                         Selenide.switchTo().window(1);
                         SelenideElement table = $x("//*[@id='start-video']/tbody/tr[2]/td");
                         SelenideElement timerInVideo = $x("//*[contains(@class, 'timer')]");
-                        SelenideElement pageIdForError = $x("//body[contains(@id, 'page')]");
+                        //SelenideElement pageIdForError = $x("//body[contains(@id, 'page')]");
                         //SelenideElement errorInVideo = $x("//div[@class = 'ytp-error-icon-container']");
 
                         Selenide.sleep(3000);
-                        WebDriverWait wait = new WebDriverWait(getWebDriver(), Duration.ofSeconds(3));
-                        boolean a = !wait.until(ExpectedConditions.elementToBeClickable(table)).isEnabled();
-                        if (a) {
+                        //WebDriverWait wait = new WebDriverWait(getWebDriver(), Duration.ofSeconds(3));
+                        if (!table.isDisplayed()) {
                             //if (Selenide.switchTo().frame("video-start").findElement(By.xpath("//div[@class = 'ytp-error-icon-container']")).isDisplayed()){
 //                    if(Selenide.switchTo().frame("video-start").findElement(By.xpath("//div[@class = 'ytp-error-icon-container']")).isDisplayed()){
                             switchTo().window(getWebDriver().getWindowHandle()).close();
                             switchTo().window(0);
                             Selenide.refresh();
+                            Selenide.sleep(2000);
                         } else {
                             table.scrollTo().click();
                             Selenide.sleep(timeWaiting);
@@ -121,9 +124,9 @@ public class Urod {
     }
 
     public static int timeForWaiting(){
-        if (timeForWatchingText.getText().contains("10")) {
+        if (timeForWatchingText.getText().contentEquals("10 сек   ")) {
             return 14000;
-        } else if (timeForWatchingText.getText().contains("15")) {
+        } else if (timeForWatchingText.getText().contentEquals("15 сек   ")) {
             return 19000;
         } else if (timeForWatchingText.getText().contains("20")) {
             if(timeForWatchingText.getText().contains("1")){
@@ -132,26 +135,30 @@ public class Urod {
                 return 24000;
             }
         } else if (timeForWatchingText.getText().contains("25")) {
-            return 29000;
+            if(timeForWatchingText.getText().contains("0")){
+                return 254000;
+            }else {
+                return 29000;
+            }
         } else if (timeForWatchingText.getText().contentEquals("300 сек   ")) {
-            return 304000;
+            return 340000;
         } else if (timeForWatchingText.getText().contentEquals("30 сек   ")) {
             return 34000;
-        } else if (timeForWatchingText.getText().contains("35")) {
+        } else if (timeForWatchingText.getText().contentEquals("35 сек   ")) {
             return 39000;
-        }else if (timeForWatchingText.getText().contains("45")) {
+        }else if (timeForWatchingText.getText().contentEquals("45 сек   ")) {
             return 49000;
-        }else if (timeForWatchingText.getText().contains("55")) {
+        }else if (timeForWatchingText.getText().contentEquals("55 сек   ")) {
             return 59000;
-        }else if (timeForWatchingText.getText().contains("50")) {
+        }else if (timeForWatchingText.getText().contentEquals("50 сек   ")) {
             return 54000;
-        }else if (timeForWatchingText.getText().contains("65")) {
+        }else if (timeForWatchingText.getText().contentEquals("65 сек   ")) {
             return 69000;
-        }else if (timeForWatchingText.getText().contains("60")) {
+        }else if (timeForWatchingText.getText().contentEquals("60 сек   ")) {
             return 64000;
-        }else if (timeForWatchingText.getText().contains("70")) {
+        }else if (timeForWatchingText.getText().contentEquals("70 сек   ")) {
             return 74000;
-        }else if (timeForWatchingText.getText().contains("75")) {
+        }else if (timeForWatchingText.getText().contentEquals("75 сек   ")) {
             return 79000;
         }else if (timeForWatchingText.getText().contains("80")) {
             if(timeForWatchingText.getText().contains("1")){
@@ -159,25 +166,25 @@ public class Urod {
             }else {
                 return 84000;
             }
-        }else if (timeForWatchingText.getText().contains("85")) {
+        }else if (timeForWatchingText.getText().contentEquals("85 сек   ")) {
             return 89000;
-        }else if (timeForWatchingText.getText().contains("90")) {
+        }else if (timeForWatchingText.getText().contentEquals("90 сек   ")) {
             return 94000;
-        }else if (timeForWatchingText.getText().contains("95")) {
+        }else if (timeForWatchingText.getText().contentEquals("95 сек   ")) {
             return 99000;
-        }else if (timeForWatchingText.getText().contains("105")) {
+        }else if (timeForWatchingText.getText().contentEquals("105 сек   ")) {
             return 109000;
-        }else if (timeForWatchingText.getText().contains("110")) {
+        }else if (timeForWatchingText.getText().contentEquals("110 сек   ")) {
             return 114000;
-        }else if (timeForWatchingText.getText().contains("115")) {
+        }else if (timeForWatchingText.getText().contentEquals("115 сек   ")) {
             return 119000;
-        }else if (timeForWatchingText.getText().contains("125")) {
+        }else if (timeForWatchingText.getText().contentEquals("125 сек   ")) {
             return 129000;
-        }else if (timeForWatchingText.getText().contains("130")) {
+        }else if (timeForWatchingText.getText().contentEquals("130 сек   ")) {
             return 134000;
-        }else if (timeForWatchingText.getText().contains("135")) {
+        }else if (timeForWatchingText.getText().contentEquals("135 сек   ")) {
             return 139000;
-        }else if (timeForWatchingText.getText().contains("14")) {
+        }else if (timeForWatchingText.getText().contentEquals("140 сек   ")) {
             return 144000;
         }else if (timeForWatchingText.getText().contains("40")) {
             if(timeForWatchingText.getText().contains("2")) {
